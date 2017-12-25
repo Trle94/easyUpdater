1edf52aeb87135d54ab1ac9c820864e8a718f741
Marko <Marko@DESKTOP-8I13A2T>
2017-12-25 17:43:37 +0100

    Adding configuration for 'EXAMPLE' Updating .Net framework of project

--- Example/Form1.cs
+++ Example/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace Example
@@ -8,11 +10,46 @@ namespace Example
         public Form1()
         {
             InitializeComponent();
+            label1.Text = ApplicationAssembly.GetName().Version.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
         }
+
+        #region Conf Update
+
+        public string ApplicationName
+        {
+            get { return "Example"; }
+        }
+
+        public string ApplicationId
+        {
+            get { return "Example"; }
+        }
+
+        public Assembly ApplicationAssembly
+        {
+            get { return Assembly.GetExecutingAssembly(); }
+        }
+
+        public Icon ApplicationIcon
+        {
+            get { return Icon; }
+        }
+
+        public Uri UpdateFileLocation
+        {
+            get { return new Uri("http://192.168.0.1/data.json"); }
+        }
+
+        public Form Context
+        {
+            get { return this; }
+        }
+
+        #endregion
     }
 }
